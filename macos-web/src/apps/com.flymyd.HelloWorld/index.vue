832f6c52d938efab6704f1be1efb553598adccde
flymyd <2663797538@qq.com>
2022-06-14 17:22:36 +0800

    test connection

--- macos-web/src/apps/com.flymyd.HelloWorld/index.vue
+++ macos-web/src/apps/com.flymyd.HelloWorld/index.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="mac-app-container com-flymyd-HelloWorld">
-    
+  <div class="mac-app-container com-flymyd-HelloWorld" @click="refresh">
+
   </div>
 </template>
 
-<script setup lang="ts" name="com.flymyd.HelloWorld">
-
+<script lang="ts" setup name="com.flymyd.HelloWorld">
+import { ref } from '@vue/reactivity';
+const refresh: any = () => {
+  (function(){
+    console.log(114)
+  })()
+}
 </script>
 
 <style scoped lang="scss">
 @import "@/assets/css/mac/AppDefault.scss";
 
-.com-flymyd-HelloWorld {
-  
-}
+.com-flymyd-HelloWorld {}
 </style>
